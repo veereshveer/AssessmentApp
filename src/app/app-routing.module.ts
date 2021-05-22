d336d4f6646ebbf9351acdab1cd6576c8744ce96
veeresh r <veereshr87@gmail.com>
2021-05-22 17:35:32 +0530

    Added admin login routing in router.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 const routes: Routes = [
+  {
+    path: 'adminLogin',
+    component: AdminLoginComponent
+
+  },
   {
     path: '',
     component: AdminHomeComponent
