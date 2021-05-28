d60746e8e1c4f43372912edac394b7a00552a65d
sachinentomo <80520688+sachinentomo@users.noreply.github.com>
2021-05-28 12:02:05 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import AdminService from './admin-login/admin.service';
-import { AuthInterCeptor } from './interceptor/auth.interceptor';
-import { AuthenticationGuardGuard } from './guard/authentication-guard.guard';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLoginComponent,
     HeaderComponent,
     FooterComponent,
     AdminHomeComponent,
@@ -33,9 +28,7 @@ import { AuthenticationGuardGuard } from './guard/authentication-guard.guard';
     HttpClientModule
 
   ],
-  providers: [AdminService, AuthenticationGuardGuard,
-              { provide: HTTP_INTERCEPTORS, useClass: AuthInterCeptor, multi: true }
-             ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
